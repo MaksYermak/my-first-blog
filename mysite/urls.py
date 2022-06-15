9799e6db3c217aea4ddefcc78b7d4e4af07bb386
MaksYermak <maks.yermakou@gmail.com>
2022-06-15 21:27:52 +0200

    Update code to Django 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-   url(r'^admin/', include(admin.site.urls)),
-   url(r'^accounts/login/$', views.login, name='login'),
-   url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+   url(r'^admin/', admin.site.urls),
+   url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+   url(r'^accounts/logout/$', views.LogoutView.as_view(next_page='/'), name='logout'),
    url(r'', include('blog.urls')),
 ]
